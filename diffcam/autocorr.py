c046a2c8b218ffe4fa802b91dc2e4b4f129ada28
Alec Flowers <alec.flowers00@gmail.com>
2021-12-12 12:52:09 +0100

    feat: autocorr function:

--- diffcam/autocorr.py
+++ diffcam/autocorr.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.signal import convolve2d
 
 def autocorr2d(vals, pad_mode="reflect"):
     """
@@ -17,4 +17,9 @@ def autocorr2d(vals, pad_mode="reflect"):
     autocorr : py:class:`~numpy.ndarray`
     """
 
-    return np.ones_like(vals)
+    n = vals.shape[0]
+    m = vals.shape[1]
+    vals_pad = np.pad(vals, [(0,n-1),(0,m-1)], mode=pad_mode)
+    vals_fft = np.fft.fft2(vals_pad)
+    return np.real(np.fft.fftshift(np.fft.ifft2(vals_fft*np.conjugate(vals_fft))))[:, int(m/2):-int(m/2)]
+
