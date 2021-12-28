e72652affaee0cac7af36b6939f4b592bcf09862
Alec Flowers <alec.flowers00@gmail.com>
2021-12-28 23:35:54 +0100

    fix: small logging fixes

--- diffcam/metric.py
+++ diffcam/metric.py
@@ -105,7 +105,7 @@ class LogMetrics():
 
     def save_logs(self):
         timestamp = datetime.now().strftime("_%d%m%d%Y_%Hh%M")
-        path = os.path.join(str(LOGPATH), f"{self.save['algo']}_{self.save['lambda']}{timestamp}")
+        path = os.path.join(str(LOGPATH), f"{self.save['algo']}_{self.save['lambda']}_{self.save['delta']}{timestamp}")
         print(f"Logs saved to: {path}")
         with open(path+".pkl", 'wb') as f:
             pickle.dump(self.save, f, pickle.HIGHEST_PROTOCOL)
