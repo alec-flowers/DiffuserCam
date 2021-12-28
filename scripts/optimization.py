3feae08fcb921e6fe3eca0b4f1ef26e50d202e7f
OhLeMatt <matthieu.verdet@epfl.ch>
2021-12-28 17:02:52 +0100

    Factorzied optimization techniques

--- scripts/optimization.py
+++ scripts/optimization.py
@@ -3,10 +3,10 @@ import time
 from pycsou.linop.conv import Convolve2D
 from pycsou.func.loss import SquaredL2Loss
 from pycsou.func.penalty import SquaredL2Norm, L2Norm, L1Norm, NonNegativeOrthant
-from pycsou.opt.proxalgs import APGD, PDS
+from pycsou.opt.proxalgs import APGD, H, PDS
 from pycsou.linop.diff import Gradient
 
-from scripts.functionals import DCT2, HuberNorm
+from scripts.functionals import DCT2, HuberNorm, OptiConvolve2D
 from scipy.fftpack import dctn, idctn
 
 
@@ -122,7 +122,7 @@ def pls(psf, data, n_iter):
     F = l22_loss * Hop  # Differentiable function
     G = NonNegativeOrthant(dim=Hop.shape[0])
     H = lambda_ * L1Norm(dim=D.shape[0])
-
+    
     pds = PDS(dim=Hop.shape[0], F=F, G=G, H=H, K=D, verbose=None,
                 min_iter=1, max_iter=n_iter, accuracy_threshold=0.0001)  
     print(f"setup time : {time.time() - start_time} s")
@@ -145,7 +145,6 @@ def pls_huber(psf, data, n_iter):
     D = Gradient(shape = data.shape)
     D.compute_lipschitz_cst(tol=5e-1)
 
-
     lambda_ = 0.01
     delta = 5
     l22_loss = (1 / 2) * SquaredL2Loss(dim=Hop.shape[0], data=data.flatten())
@@ -161,3 +160,54 @@ def pls_huber(psf, data, n_iter):
     print(f"proc time : {time.time() - start_time} s")
 
     return estimate, converged, diagnostics
+
+def optimize(method, psf, data, n_iter, lambda_=0.1, delta=None):
+    start_time = time.time()
+    runner = get_runner(method, psf, data, n_iter, lambda_, delta)
+    print(f"setup time : {time.time() - start_time} s")
+    
+    start_time = time.time()
+    estimate, converged, diagnostics = runner.iterate()
+    print(f"proc time : {time.time() - start_time} s")
+
+    return estimate, converged, diagnostics
+
+def get_runner(method, psf, data, n_iter, lambda_=0.1, delta=None):
+    Hop = OptiConvolve2D(psf)
+    Hop.compute_lipschitz_cst(tol=5e-1)
+    loss = (1 / 2) * SquaredL2Loss(dim=Hop.shape[0], data=data.flatten())
+    F = loss * Hop
+    G = None
+    H = None
+    D = None
+    if method == "ridge":
+        F += lambda_ * SquaredL2Norm(Hop.shape[0])
+    elif method == "lasso":
+        G = lambda_ * L1Norm(Hop.shape[0])
+    elif method == "glasso":
+        dct2 = DCT2(data.shape)
+        idct2 = dct2.get_adjointOp()
+        idct2.compute_lipschitz_cst(tol=5e-1)
+        F *= idct2
+        G = lambda_ * L1Norm(Hop.shape[0])
+    elif method == "nnls":
+        G = lambda_ * NonNegativeOrthant(Hop.shape[0])
+    elif method == "pls":
+        D = Gradient(shape = data.shape)
+        D.compute_lipschitz_cst(tol=5e-1)
+        H = lambda_ * L1Norm(dim=D.shape[0])
+        G = NonNegativeOrthant(dim=Hop.shape[0])
+    elif method == "pls_huber":
+        if delta is None:
+            raise ValueError("Delta should be defined for huber")
+        D = Gradient(shape = data.shape)
+        D.compute_lipschitz_cst(tol=5e-1)
+        F += lambda_ * HuberNorm(dim=D.shape[0], delta=delta) * D  # Differentiable function
+        G = NonNegativeOrthant(dim=Hop.shape[0])
+    
+    if method in ["pls"]:
+        return PDS(dim=Hop.shape[0], F=F, G=G, H=H, K=D, verbose=None,
+                    min_iter=1, max_iter=n_iter, accuracy_threshold=0.0001)
+    else:
+        return APGD(dim=Hop.shape[1], F=F, G=G, acceleration='CD', verbose=None,
+                min_iter=1, max_iter=n_iter, accuracy_threshold=0.0001)
