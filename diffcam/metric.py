7d8332e6d9b7287df97554b18093329d2e7fb5ca
Albert Kjøller Jacobsen <albert_k_jacobsen@hotmail.com>
2021-12-28 12:40:01 +0100

    grayscale evaluation script done

--- diffcam/metric.py
+++ diffcam/metric.py
@@ -18,27 +18,24 @@ def psnr(true, est, normalize=True):
     return peak_signal_noise_ratio(image_true=true, image_test=est)
 
 
-def ssim(true, est, normalize=True):
+def ssim(true, est, normalize=True, channel_axis=2): #changed to account for grayscale input
     if normalize:
         true /= true.max()
         est /= est.max()
-    return structural_similarity(im1=true, im2=est, channel_axis=2)
+    return structural_similarity(im1=true, im2=est, channel_axis=channel_axis)
 
 
-def lpips(true, est, normalize=True):
+def lpips(true, est, normalize=True, gray=False):
     # https://github.com/richzhang/PerceptualSimilarity
     if normalize:
         true /= true.max()
         est /= est.max()
     loss_fn = lpips_lib.LPIPS(net="alex", verbose=False)
-    true = torch.from_numpy(
-        np.transpose(true, axes=(2, 0, 1))[
-            np.newaxis,
-        ].copy()
-    )
-    est = torch.from_numpy(
-        np.transpose(est, axes=(2, 0, 1))[
-            np.newaxis,
-        ].copy()
-    )
+    if gray: #TODO: Check this thing! Not sure :-)
+        true = torch.from_numpy(true[np.newaxis,].copy()).float()
+        est = torch.from_numpy(est[np.newaxis,].copy()).float()
+    else:
+        true = torch.from_numpy(np.transpose(true, axes=(2, 0, 1))[np.newaxis,].copy())
+        est = torch.from_numpy(np.transpose(est, axes=(2, 0, 1))[np.newaxis,].copy())
+
     return loss_fn.forward(true, est).squeeze().item()
--- scripts/evaluation_bash.py
+++ scripts/evaluation_bash.py
@@ -1,10 +1,17 @@
 """
 
+```bash
+
+python scripts/evaluation_bash.py --data our_images --psf_fp data/psf/diffcam_rgb.png --algo ridge --n_iter 500 --gray --plot --save
+
+```
 """
+
 import os
 import cv2
 import time
 import glob
+import click
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -16,6 +23,93 @@ from diffcam.util import DATAPATH, RECONSTRUCTIONPATH, print_image_info, resize,
 
 from scripts.optimization import lasso, ridge, nnls, glasso, pls, pls_huber
 
+
+@click.command()
+@click.option(
+    "--data",
+    type=str,
+    default='our_images',
+    help="Specify the data set to use."
+)
+@click.option(
+    "--psf_fp",
+    type=str,
+    help="File name for recorded PSF.",
+)
+@click.option(
+    "--n_files",
+    type=int,
+    default=None,
+    help="Evaluates first n files. None yields all",
+)
+@click.option("--algo",
+              type=str,
+              help="Name of reconstruction algorithm.",
+              )
+@click.option(
+    "--n_iter",
+    type=int,
+    default=500,
+    help="Number of iterations.",
+)
+@click.option(
+    "--downsample",
+    type=float,
+    default=4,
+    help="Downsampling factor.",
+)
+@click.option(
+    "--disp",
+    default=50,
+    type=int,
+    help="How many iterations to wait for intermediate plot/results. Set to negative value for no intermediate plots.",
+)
+@click.option(
+    "--flip",
+    is_flag=True,
+    help="Whether to flip image.",
+)
+@click.option(
+    "--save",
+    is_flag=True,
+    help="Whether to save intermediate and final reconstructions.",
+)
+@click.option(
+    "--gray",
+    is_flag=True,
+    help="Whether to perform construction with grayscale.",
+)
+@click.option(
+    "--bayer",
+    is_flag=True,
+    help="Whether image is raw bayer data.",
+)
+@click.option(
+    "--plot",
+    is_flag=True,
+    help="Whether to no plot.",
+)
+@click.option(
+    "--bg",
+    type=float,
+    help="Blue gain.",
+)
+@click.option(
+    "--rg",
+    type=float,
+    help="Red gain.",
+)
+@click.option(
+    "--gamma",
+    default=None,
+    type=float,
+    help="Gamma factor for plotting.",
+)
+@click.option(
+    "--single_psf",
+    is_flag=True,
+    help="Same PSF for all channels (sum) or unique PSF for RGB.",
+)
 def evaluate(data,
              n_files,
              psf_fp,
@@ -204,38 +298,4 @@ def evaluate(data,
 
 
 if __name__ == '__main__':
-    data = 'our_images'
-    n_files = None          # None yields all :-)
-    algo = 'ridge'
-    n_iter = 1
-    gray = True
-    downsample = 4
-    disp = 50
-    flip = False
-    bayer = False
-    bg = None
-    rg = None
-    gamma = None
-    save = True
-    plot = True
-    single_psf = False
-
-    psf_fp = rf'{str(DATAPATH)}{os.sep}psf{os.sep}diffcam_rgb.png'
-
-    evaluate(data,
-             n_files,
-             psf_fp,
-             algo,
-             n_iter,
-             downsample,
-             disp,
-             flip,
-             gray,
-             bayer,
-             bg,
-             rg,
-             gamma,
-             save,
-             plot,
-             single_psf
-             )
+    evaluate()
