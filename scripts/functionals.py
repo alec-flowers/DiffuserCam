9f6bb5890bd91c1415da883761bd042be030ebb1
OhLeMatt <matthieu.verdet@epfl.ch>
2021-12-28 02:51:51 +0100

    Added a version of customized Convolve2D version, which is not fully optimized yet apparently

--- scripts/functionals.py
+++ scripts/functionals.py
@@ -1,5 +1,6 @@
 from numbers import Number
 from typing import Union
+from scipy import fft
 
 from pycsou.core import LinearOperator
 from pycsou.core.functional import DifferentiableFunctional
@@ -19,6 +20,37 @@ class DCT2(LinearOperator):
     def adjoint(self, y: np.ndarray) -> np.ndarray:
         return idctn(y.reshape(self.origshape), type = 2 , norm = 'ortho').flatten()
 
+class OptiConvolve2D(LinearOperator):
+    def __init__(self, psf:np.ndarray):
+        super(OptiConvolve2D, self).__init__(shape=(psf.size, psf.size), dtype=psf.dtype)
+        self.origshape = np.array(psf.shape)
+        self.fshape2d = np.array(psf.shape)[0:2]*2 - 1
+        
+        offset_conv = np.zeros_like(psf.shape)
+        offset_conv[0:2] = self.origshape[0:2]//2
+        offset_corr = offset_conv.copy()
+        offset_conv[0:2] -= (self.origshape[0:2] + 1)%2
+        self.fslice2d_conv = tuple([slice(off, off + sz) for off, sz in zip(offset_conv, psf.shape)])
+        self.fslice2d_corr = tuple([slice(off, off + sz) for off, sz in zip(offset_corr, psf.shape)])
+        
+        self.fft_psf = fft.rfftn(psf, self.fshape2d, axes=(0,1))
+        psf_conj = psf[::-1,::-1].conj()
+        self.fft_psf_conj = fft.rfftn(psf_conj, self.fshape2d, axes=(0,1))
+        
+        
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        x = np.reshape(x, self.origshape)
+        x = fft.rfftn(x, self.fshape2d, axes=(0,1))
+        y = fft.irfftn(x * self.fft_psf, self.fshape2d, axes=(0,1))
+        return y[self.fslice2d_conv].flatten()
+    
+    # Function almost exactly repeated to avoid branching
+    def adjoint(self, y: np.ndarray) -> np.ndarray:
+        y = np.reshape(y, self.origshape)
+        y = fft.rfftn(y, self.fshape2d, axes=(0,1))
+        x = fft.irfftn(y * self.fft_psf_conj, self.fshape2d, axes=(0,1))    
+        return x[self.fslice2d_corr].flatten()
+        
 
 class HuberNorm(DifferentiableFunctional):
     def __init__(self, dim: int, delta: float):
